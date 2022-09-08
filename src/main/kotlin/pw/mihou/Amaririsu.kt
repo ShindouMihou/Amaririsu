f8ea4b1e39d14ef1a0e71cd8f7653adf9a641efb
Shindou Mihou <git@mihou.pw>
2022-09-09 03:12:00 +0800

    Resolution for charset is not expected.

--- src/main/kotlin/pw/mihou/Amaririsu.kt
+++ src/main/kotlin/pw/mihou/Amaririsu.kt
@@ -19,7 +19,6 @@ import pw.mihou.parsers.options.modules.SearchOptions
 import pw.mihou.parsers.options.modules.SeriesOptions
 import pw.mihou.parsers.options.modules.UserOptions
 import java.net.URLEncoder
-import java.nio.charset.Charset
 
 object Amaririsu {
 
@@ -101,7 +100,7 @@ object Amaririsu {
                 builder
             }
 
-            val search = URLEncoder.encode(name, Charset.forName("UTF-8"))
+            val search = URLEncoder.encode(name, "UTF-8")
 
             cache(
                 url = "https://www.scribblehub.com/?s=$search&post_type=fictionposts$additionalParameters",
